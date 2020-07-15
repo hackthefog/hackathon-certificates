8714dabb373800a816562941a07d4aac191783f3
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-07-14 19:06:22 -0700

    Hashing updated

--- javascript.js
+++ javascript.js
@@ -1,72 +1,121 @@
 function checkCert() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const name = urlParams.get("name");
-  const role = urlParams.get("role");
-  const hash = urlParams.get("key");
+   const urlParams = new URLSearchParams(window.location.search);
 
-  console.log(urlParams);
-  console.log(name);
-  console.log(role);
-  console.log(hash);
+   //mandatory params
+   const type = urlParams.get("type");
+   const hash = urlParams.get("key");
 
-  var combined = name + role;
-
-  var correctHash = 0;
-  if (combined.length != 0) {
-    for (i = 0; i < combined.length; i++) {
-      char = combined.charCodeAt(i);
-      correctHash = (correctHash << 5) - correctHash + char;
-      correctHash = correctHash & correctHash;
-    }
-  }
-  correctHash += ""; //turn into string
-  console.log("correctHash: " + correctHash);
-
-  if (correctHash == hash) {
-    //wait until elements exist
-    var observer = new MutationObserver(function (mutations, me) {
-      var elements = [
-        document.getElementById("cert"),
-        document.getElementById("cert-temp"),
-      ];
-      var missing = false;
-      for (var i = 0; i < elements.length; i++) {
-        if (!elements[i]) {
-          missing = true;
-        }
+   var allParams = [];
+   var flattenedParams = [];
+   for (var entry of urlParams.entries()) {
+      if (entry[0] != "key") {
+         allParams.push(entry);
+         flattenedParams.push(entry[0]);
+         flattenedParams.push(entry[1]);
       }
-      if (!missing) {
-        var certHolder = document.getElementById("cert");
-        var cert = document.getElementById("cert-temp").content.cloneNode(true);
-        certHolder.innerHTML = "";
-        certHolder.appendChild(cert);
-        document.getElementById("name").innerText = name;
-        document.getElementById("role").innerText = role;
+   }
 
-        destroyCertTemp();
-        me.disconnect(); // stop observing
-        return;
+   var correctHash = keyHash(flattenedParams);
+	console.log("Correct Hash: " + correctHash);
+
+   if (!(type || hash)) {
+      console.log("no params");
+      window.addEventListener
+         ? window.addEventListener("load", homePage, false)
+         : window.attachEvent && window.attachEvent("onload", homePage);
+   } else if (hash == correctHash) {
+      //wait until elements exist
+      var observer = new MutationObserver(function (mutations, me) {
+         var elements = [document.getElementById("cert")];
+         var missing = false;
+         for (var i = 0; i < elements.length; i++) {
+            if (!elements[i]) {
+               missing = true;
+            }
+         }
+         if (!missing) {
+            var path = "external/" + type + ".html";
+
+            var request = new XMLHttpRequest();
+            request.open("GET", path, true);
+            request.onload = function () {
+               if (request.status >= 200 && request.status < 400) {
+                  var resp = request.responseText;
+                  var temp = document.createElement("div");
+                  temp.innerHTML = resp;
+                  document.querySelector("#cert").innerHTML = resp;
+                  // document.querySelector("#cert").appendChild(temp);
+                  for (var entry of allParams) {
+                     var elements = document.querySelectorAll(
+                        "[cert-replace=" + entry[0] + "]"
+                     );
+                     for (var element of elements) {
+                        element.innerText = entry[1];
+                     }
+                  }
+                  certResize();
+                  snapshot();
+               } else {
+                  error();
+               }
+            };
+            request.send();
+
+            me.disconnect();
+            return;
+         }
+      });
+
+      observer.observe(document, {
+         childList: true,
+         subtree: true,
+      });
+   } else {
+      console.log("invalid cert");
+      window.addEventListener
+         ? window.addEventListener("load", invalidCert, false)
+         : window.attachEvent && window.attachEvent("onload", invalidCert);
+   }
+
+   function keyHash(params) {
+      //[key1, value1, key2, value2, ...]
+		console.log(params);
+
+      if (params.length % 2 != 0) {
+         return (
+            "ERROR: odd number of arguments (" +
+            params.length +
+            ") [" +
+            Math.random() +
+            "]"
+         ); //random number prevents correct hash
       }
-    });
+      var correctHash = 0;
+      for (var i = 0; i < params.length; i += 2) {
+         correctHash +=
+            parseInt(hashParam(params[i] + "|" + params[i + 1])) /
+            params.length;
+      }
+      correctHash = hashParam(correctHash);
+      return correctHash;
 
-    observer.observe(document, {
-      childList: true,
-      subtree: true,
-    });
-  } else {
-    console.log("incorrect");
-    window.onload = function () {
-      var certHolder = document.getElementById("cert");
-      var error = document.getElementById("error").content.cloneNode(true);
-      certHolder.innerHTML = "";
-      certHolder.appendChild(error);
+      function hashParam(input) {
+         input += ""; //convert to string
+         var output = 0;
+         if (input.length != 0) {
+            for (var i = 0; i < input.length; i++) {
+               var char = input.charCodeAt(i);
+               output = (output << 5) - output + char;
+               output = output & output;
+            }
+         }
+         return output;
+      }
+   }
+}
 
-		destroyCertTemp();
-		
-		var fab = document.getElementsByClassName("fab")[0];
-		fab.style.display = "none";
-    };
-  }
+function invalidCert() {
+   console.log("loaded invalidCert");
 }
 
 function destroyCertTemp() {
