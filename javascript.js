a33cd3b24de5b15eda13982fa10df82f0fbbc3dc
calee14 <caplee1000@gmail.com>
2020-07-22 15:11:41 -0700

    try await

--- javascript.js
+++ javascript.js
@@ -17,99 +17,92 @@ function checkCert() {
         }
     }
 
-	getHash(name, role, type, function(correctHash) {
-		console.log(hash);
-		console.log(correctHash);
+	var correctHash = await getHash(name, role, type);
 
-		if (!(type || hash)) {
-			console.log("no params");
-			window.addEventListener
-				? window.addEventListener("load", homePage, false)
-				: window.attachEvent && window.attachEvent("onload", homePage);
-		} else if (hash.localeCompare(correctHash) == 0) {
-			addCertificate(allParams, type)
-		} else {
-			console.log("invalid cert");
-			window.addEventListener
-				? window.addEventListener("load", invalidCert, false)
-				: window.attachEvent && window.attachEvent("onload", invalidCert);
-		}
-	});
-}
 
-function addCertificate(allParams, type) {
-	console.log("we're in");
-	//wait until elements exist
-	var observer = new MutationObserver(function (mutations, me) {
-		var elements = [document.getElementById("cert")];
-		var missing = false;
-		for (var i = 0; i < elements.length; i++) {
-			if (!elements[i]) {
-				missing = true;
-			}
-		}
-		console.log(missing);
-		if (!missing) {
-			console.log("deeper");
-			var path = "external/" + type + ".html";
-
-			var request = new XMLHttpRequest();
-			request.open("GET", path, true);
-			request.onload = function () {
-				console.log("deepest");
-				if (request.status >= 200 && request.status < 400) {
-					var resp = request.responseText;
-					var temp = document.createElement("div");
-					temp.innerHTML = resp;
-					document.querySelector("#cert").innerHTML = resp;
-					// document.querySelector("#cert").appendChild(temp);
-					for (var entry of allParams) {
-						var elements = document.querySelectorAll(
-							"[cert-replace=" + entry[0] + "]"
-						);
-						for (var element of elements) {
-							element.innerText = entry[1];
-						}
-					}
-					console.log("add cert");
-					document.querySelector("#name").innerText = urlParams.get("name") || "Hacker";
-					document.querySelector("#role").innerText = urlParams.get("role") || "For Attending";
-					certResize();
-					snapshot();
-				} else {
-					error();
+	if (!(type || hash)) {
+		console.log("no params");
+		window.addEventListener
+			? window.addEventListener("load", homePage, false)
+			: window.attachEvent && window.attachEvent("onload", homePage);
+	} else if (hash.localeCompare(correctHash) == -1) {
+		//wait until elements exist
+		var observer = new MutationObserver(function (mutations, me) {
+			var elements = [document.getElementById("cert")];
+			var missing = false;
+			for (var i = 0; i < elements.length; i++) {
+				if (!elements[i]) {
+					missing = true;
 				}
-			};
-			request.send();
+			}
+			if (!missing) {
+				var path = "external/" + type + ".html";
 
-			me.disconnect();
-			return;
-		}
-	});
+				var request = new XMLHttpRequest();
+				request.open("GET", path, true);
+				request.onload = function () {
+					if (request.status >= 200 && request.status < 400) {
+						var resp = request.responseText;
+						var temp = document.createElement("div");
+						temp.innerHTML = resp;
+						document.querySelector("#cert").innerHTML = resp;
+						// document.querySelector("#cert").appendChild(temp);
+						for (var entry of allParams) {
+							var elements = document.querySelectorAll(
+								"[cert-replace=" + entry[0] + "]"
+							);
+							for (var element of elements) {
+								element.innerText = entry[1];
+							}
+						}
+						document.querySelector("#name").innerText = urlParams.get("name") || "Hacker";
+						document.querySelector("#role").innerText = urlParams.get("role") || "For Attending";
+						certResize();
+						snapshot();
+					} else {
+						error();
+					}
+				};
+				request.send();
 
-	observer.observe(document, {
-		childList: true,
-		subtree: true,
-	});
-}
+				me.disconnect();
+				return;
+			}
+		});
 
-function getHash(name, role, typ, callback) {
-	const url = "https://cors-anywhere.herokuapp.com/https://certhasher.herokuapp.com/hash/verify";
+		observer.observe(document, {
+			childList: true,
+			subtree: true,
+		});
+	} else {
+		console.log("invalid cert");
+		window.addEventListener
+			? window.addEventListener("load", invalidCert, false)
+			: window.attachEvent && window.attachEvent("onload", invalidCert);
+	}
 
-	$.ajax({
-	    url: url,
-	    type: "GET", //send it through get method
-	    data: { 
-	        name: name, 
-	        role: role, 
-	        type: typ
-	    },
-	    success: callback,
-	    error: function(xhr) {
-	        //Do Something to handle error
-	        console.log(xhr)
-	    }
-	});
+	function getHash(name, role, typ) {
+		const url = "https://cors-anywhere.herokuapp.com/https://certhasher.herokuapp.com/hash/verify";
+
+		$.ajax({
+		    url: url,
+		    type: "GET", //send it through get method
+		    data: { 
+		        name: name, 
+		        role: role, 
+		        type: typ
+		    },
+		    success: function(response) {
+		        //Do Something
+		        console.log(response);
+		        return String(response);
+		    },
+		    error: function(xhr) {
+		        //Do Something to handle error
+		        console.log(xhr)
+		    }
+		});
+	}
 }
 
 function homePage() {
