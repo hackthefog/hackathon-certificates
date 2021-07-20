f49028b411e9daaf23f0220f05784df31abae61e
Marvin Lee <43216761+malee31@users.noreply.github.com>
2021-07-19 18:12:04 -0700

    Now renders properly. Redid the JS to make it shorter and only kept the necessary bits

--- static/javascript.js
+++ static/javascript.js
@@ -1,132 +1,3 @@
-function checkCert() {
-	const urlParams = new URLSearchParams(window.location.search);
-
-	//mandatory params
-	const type = urlParams.get("type");
-	const hash = urlParams.get("key");
-
-	let allParams = [];
-	let flattenedParams = [];
-	for(let entry of urlParams.entries()) {
-		if(entry[0] !== "key") {
-			allParams.push(entry);
-			flattenedParams.push(entry[0]);
-			flattenedParams.push(entry[1]);
-		}
-	}
-
-	let correctHash = keyHash(flattenedParams);
-
-	if(!(type || hash)) {
-		console.log("no params");
-		window.addEventListener
-			? window.addEventListener("load", homePage, false)
-			: window.attachEvent && window.attachEvent("onload", homePage);
-	} else if(hash === correctHash) {
-		//wait until elements exist
-		let observer = new MutationObserver(function(mutations, me) {
-			let elements = [document.getElementById("cert")];
-			let missing = false;
-			for(let i = 0; i < elements.length; i++) {
-				if(!elements[i]) {
-					missing = true;
-				}
-			}
-			if(!missing) {
-				let path = "external/" + type + ".html";
-
-				let request = new XMLHttpRequest();
-				request.open("GET", path, true);
-				request.onload = function() {
-					if(request.status >= 200 && request.status < 400) {
-						let resp = request.responseText;
-						let temp = document.createElement("div");
-						temp.innerHTML = resp;
-						document.querySelector("#cert").innerHTML = resp;
-						// document.querySelector("#cert").appendChild(temp);
-						for(let entry of allParams) {
-							let elements = document.querySelectorAll(
-								"[cert-replace=" + entry[0] + "]"
-							);
-							for(let element of elements) {
-								element.innerText = entry[1];
-							}
-						}
-						document.querySelector("#name").innerText = urlParams.get("name") || "Hacker";
-						document.querySelector("#role").innerText = urlParams.get("role") || "For Attending";
-						certResize();
-						snapshot();
-					} else {
-						error();
-					}
-				};
-				request.send();
-
-				me.disconnect();
-				return;
-			}
-		});
-
-		observer.observe(document, {
-			childList: true,
-			subtree: true,
-		});
-	} else {
-		console.log("invalid cert");
-		window.addEventListener
-			? window.addEventListener("load", invalidCert, false)
-			: window.attachEvent && window.attachEvent("onload", invalidCert);
-	}
-
-	function keyHash(params) {
-		//[key1, value1, key2, value2, ...]
-		console.log(params);
-
-		if(params.length % 2 !== 0) {
-			return (
-				"ERROR: odd number of arguments (" +
-				params.length +
-				") [" +
-				Math.random() +
-				"]"
-			); //random number prevents correct hash
-		}
-		let correctHash = 0;
-		for(let i = 0; i < params.length; i += 2) {
-			correctHash +=
-				parseInt(hashParam(params[i] + "|" + params[i + 1])) /
-				params.length;
-		}
-		correctHash = hashParam(correctHash);
-		return correctHash;
-
-		function hashParam(input) {
-			input += ""; //convert to string
-			let output = 0;
-			if(input.length !== 0) {
-				for(let i = 0; i < input.length; i++) {
-					let char = input.charCodeAt(i);
-					output = (output << 5) - output + char;
-					output = output & output;
-				}
-			}
-			return output;
-		}
-	}
-}
-
-function homePage() {
-	console.log("loaded home");
-	loadMessage("messages/home.html");
-	hideDownload();
-}
-
-function invalidCert() {
-	console.log("loaded invalidCert");
-	loadMessage("messages/invalidCert.html");
-	hideDownload();
-}
-
 function error() {
 	console.log("loaded error");
 	loadMessage("messages/error.html");
@@ -157,21 +28,13 @@ function hideDownload() {
 }
 
 function download() {
-	if(canvasSnapshot) {
-		let imgData = canvasSnapshot.toDataURL("image/jpeg", 1);
-		let doc = new jsPDF("l", "in", "letter");
-		doc.addImage(imgData, "JPEG", 0, 0, 11, 8.5);
-		doc.save("synHacks Certificate.pdf");
-	}
-}
-
-let canvasSnapshot;
-
-function snapshot() {
 	html2canvas(document.querySelector("#cert"), {
 		scale: 4,
-	}).then((canvas) => {
-		canvasSnapshot = canvas;
+	}).then(canvas => {
+		let imgData = canvas.toDataURL("image/jpeg", 1);
+		let doc = new jsPDF("l", "in", "letter");
+		doc.addImage(imgData, "JPEG", 0, 0, 11, 8.5);
+		doc.save("Hackathon Certificate.pdf");
 	});
 }
 
@@ -207,5 +70,3 @@ window.onresize = function() {
 window.addEventListener
 	? window.addEventListener("load", certResize, false)
 	: window.attachEvent && window.attachEvent("onload", certResize);
-
-checkCert();
